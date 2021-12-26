5735d67361b40af5e59b01ec4f095e7ec2934b2f
Afzal2446 <68365091+Afzal2446@users.noreply.github.com>
2021-12-26 03:32:46 -0800

    Create Solution2.js

---
+++ Solution2.js
@@ -0,0 +1,13 @@
+function printGP(a, r, n)
+{
+    let curr_term;
+    for (let i = 0; i < n; i++) {
+        curr_term = a * Math.pow(r, i);
+        console.write(curr_term + " ");
+    }
+}
+    let a = 2; // starting number
+    let r = 3; // Common ratio
+    let n = 5; // N th term to be find
+    printGP(a, r, n);
+  
